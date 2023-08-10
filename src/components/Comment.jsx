e0f124539786fefd2718ce57eb23af64a56338ea
JEXB <jippee.1211@gmail.com>
2023-08-10 14:42:41 +0700

    Quiz2

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,3 +1,5 @@
+import { Reply } from "./Reply";
+
 export const Comment = ({
   userImagePath,
   username,
@@ -7,8 +9,43 @@ export const Comment = ({
 }) => {
   return (
     <div>
-      your code here...
-      {/* map-loop render Reply component here */}
+      <div className="d-flex gap-2 my-2">
+        <img
+          src={userImagePath}
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <div
+          className="rounded rounded-3 p-2"
+          style={{ backgroundColor: "#3A3B3C" }}
+        >
+          <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+            {username}
+          </span>
+          <br />
+          <span style={{ color: "#E4E6EB" }}>{commentText}</span>
+          <div className="d-flex align-items-center gap-1">
+            <img src="/like.svg" width={20}></img>
+            <span style={{ color: "#B0B3B8" }} && {likeNum > 0 ? 'likeNum' : ""} >
+              {likeNum} คน
+            </span>
+          </div>
+        </div>
+      </div>
+      {
+        /* map-loop render Reply component here */
+        replies.map((reply) => (
+          <Reply
+            key={reply.username}
+            userImagePath={reply.userImagePath}
+            username={reply.username}
+            replyText={reply.replyText}
+            likeNum={reply.likeNum}
+          />
+        ))
+      }
     </div>
   );
 };
